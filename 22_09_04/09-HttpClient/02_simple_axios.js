c714d4abb38a063bdfdfba4e54f13f6ff3b58570
yunjin oh <dusrn2xlayjoh@gmail.com>
2022-09-22 09:50:27 +0900

    에러 메시지 출력 폼 변경

--- 22_09_04/09-HttpClient/02_simple_axios.js
+++ 22_09_04/09-HttpClient/02_simple_axios.js
@@ -2,17 +2,19 @@
 import axios from 'axios';
 
 // 2. 접속할 서버의 호스트 이름과 요청정보(path)설정
-const url = 'http://itpaper.co.kr/data/simple_text.txt';
+const url = 'http://data.hossam.kr/simple_text.txt';
 
 // 3. GET방식으로 접속하기 위한 객체 생성(promise 방식)
 axios
     .get(url)
+    // 성공했을 때
     .then(function (response) {
         // 지정된 url의 컨텐츠를 성공적으로 가져온 경우 호출됨
         // --> 응답을 성공적으로 수신했다고 표현
 
         console.log('Promise 방식 - ' + response.data);
     })
+    // 실패 했을 때
     .catch(function (error) {
         // 지정된 url로의 요청에 실패한 경우 호출됨
         // 에러 내용에서 상태코드(숫자)와 에러 메시지만 추출
@@ -22,13 +24,15 @@ axios
         //  - 401(권한없음, 로그인필요)
         //  - 403(접근금지, 폴더에 접속한 경우)
         //  - 50x(접속 대상에서 에러가 나고 있는 경우)
-        const errorMsg = '[' + error.response.status + '] ' + error.response.statusText;
-        console.log("Promise 방식 - " + errorMsg);
+        if (error.response !== undefined) {
+            const errorMsg = '[' + error.response.status + '] ' + error.response.statusText;
+            console.log("Promise 방식 - " + errorMsg);
+        }
     })
+    // 성공, 실패 여부에 상관없이 마지막에 무조건 호출됨
     .finally(function () {
-        // 성공, 실패 여부에 상관없이 마지막에 무조건 호출됨
         // 필요 없다면 이 부분은 구현하지 않아도 됨
         console.log('Promise방식 - fin :)');
     });
-
-    console.log('Promise 방식으로 HTTP 요청');
+// 가장 먼저 실행 후 결과에 따라 위에꺼 실행
+console.log('Promise 방식으로 HTTP 요청');

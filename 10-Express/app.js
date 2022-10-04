3c61e10d842a5a9c2e8b6ad09f7cfccd5f02a508
yunjin oh <dusrn2xlayjoh@gmail.com>
2022-10-04 15:53:37 +0900

    5주차_06-sendmail.js_/public/06_mail.html

--- 10-Express/app.js
+++ 10-Express/app.js
@@ -17,6 +17,7 @@ import bodyParser from "body-parser";           // POST 파라미터 처리
 import methodOverride from "method-override";   // PUT 파라미터 처리
 import cookieParser from "cookie-parser";       // Cookie  처리
 import expressSession from 'express-session';   // Session 처리
+import nodemailer from 'nodemailer';            // 메일 발송 --> app.use()로 추가설정 필요 없음.
 
 
 /*----------------------------------------------------------
@@ -478,6 +479,82 @@ router
         const json = { rt: result_msg };
         res.status(result_code).send(json);
     });
+
+
+// 06-sendmail.js
+// public/06_mail.html
+
+// 실행하기 전 config.env 파일에서
+// ex)
+//      SMTP_USERNAME = yun@gmail.com
+//      SMTP_PASSWORD = abcdefghijk
+// 작성한 후에 실행 해야함
+
+
+router.post('/send_mail', async (req, res, next) => {
+    // 1. 프론트엔드에서 전달한 사용자 입력값
+    const writer_name = req.body.writer_name;
+    let writer_email = req.body.writer_email;
+    const receiver_name = req.body.receiver_name;
+    let receiver_email = req.body.receiver_email;
+    const subject = req.body.subject;
+    const content = req.body.content;
+
+    // 2. 보내는 사람, 받는 사람의 메일주소와 이름
+    // 보내는 사람의 이름과 주소
+    //      --> 외부 SMTP연동 시 주의사항 - 발신주소가 로그인 계정과 다를 경우 발송이 거부됨.
+    if (writer_name) {
+        // ex) yun <yun@gmail.com>
+        writer_email = writer_name + '<' + writer_email + '>';
+    }
+
+    // 받는 사람의 이름과 주소
+    if (receiver_name) {
+        receiver_email = receiver_name + '<' + receiver_email + '>';
+    }
+
+    // 3. 메일 발송정보 구성
+    const send_info = {
+        from: writer_email,
+        to: receiver_email,
+        subject: subject,
+        html: content
+    };
+
+    // 4. 발송에 필요한 서버 정보를 사용하여 발송객체 생성
+    const smtp = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,            // SMTP 서버명 : smtp.gmail.com
+        port: process.env.SMTP_PORT,            // SMTP 포트   : 465
+        secure: true,                           // 보안연결(SSL) 필요
+        auth: {
+            user: process.env.SMTP_USERNAME,    // Gmail 로그인에 사용하는 메일 주소
+            pass: process.env.SMTP_PASSWORD,    // 앱 비밀번호
+        }
+    });
+    console.log({
+        host: process.env.SMTP_HOST,            // SMTP 서버명 : smtp.gmail.com
+        port: process.env.SMTP_PORT,            // SMTP 포트   : 465
+        secure: true,                           // 보안연결(SSL) 필요
+        auth: {
+            user: process.env.SMTP_USERNAME,    // Gmail 로그인에 사용하는 메일 주소
+            pass: process.env.SMTP_PASSWORD,    // 앱 비밀번호
+        }
+    });
+
+
+    // 5. 메일발송 요청
+    let rt = 200;
+    let rtMsg = "OK";
+
+    try {
+        await smtp.sendMail(send_info);
+    } catch (err) {
+        rt = 500;
+        rtMsg = err.message;
+    }
+
+    res.status(rt).send(rtMsg);
+});
 
 
 

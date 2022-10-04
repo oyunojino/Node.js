8c9e14d970af7f46c6c9c9bb508cbbf4926b18b2
yunjin oh <dusrn2xlayjoh@gmail.com>
2022-10-04 15:30:32 +0900

    5주차_05-session.js_/public/05_login.html

--- 10-Express/app.js
+++ 10-Express/app.js
@@ -410,6 +410,74 @@ router
     });
 
 
+// public/05_login.html
+router
+    .post('/session/login', (req, res, next) => {
+        const id = req.body.userid;
+        const pw = req.body.userpw;
+
+        logger.debug('id=' + id);
+        logger.debug('pw=' + pw);
+
+        let login_ok = false;
+        if (id == 'node' && pw == '1234') {
+            logger.debug('로그인 성공');
+            login_ok = true;
+        }
+
+        let result_code = null;
+        let result_msg = null;
+
+        if (login_ok) {
+            req.session.userid = id;
+            req.session.userpw = pw;
+            result_code = 200;
+            result_msg = 'ok';
+        }
+        else {
+            result_code = 403;
+            result_msg = 'fail';
+        }
+
+        const json = { rt: result_msg };
+        res.status(result_code).send(json);
+    })
+    .delete('/session/login', async (req, res, next) => {
+        let result = 'ok';
+        let code = 200;
+
+        try {
+            await req.session.destroy();
+        } catch (e) {
+            logger.error(e.message);
+            result = e.message;
+            code = 500;
+        }
+
+        const json = { rt: result };
+        res.status(code).send(json);
+    })
+    .get('/session/login', (req, res, next) => {
+        const id = req.session.userid;
+        const pw = req.session.userpw;
+
+        let result_code = null;
+        let result_msg = null;
+
+        if (id !== undefined && pw !== undefined) {
+            logger.debug('현재 로그인중이 맞습니다.');
+            result_code = 200;
+            result_msg = 'ok';
+        }
+        else {
+            logger.debug('현재 로그인중이 아닙니다.');
+            result_code = 400;
+            result_msg = 'fail';
+        }
+
+        const json = { rt: result_msg };
+        res.status(result_code).send(json);
+    });
 
 
 
